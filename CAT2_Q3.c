24b0778fc7144f56ab8dabe8a7049fa0288c4430
Taiku662 <njeritaiku@gmail.com>
2025-11-05 13:04:38 +0300

    Add files via upload

---
+++ CAT2_Q3.c
@@ -0,0 +1,25 @@
+/*
+PURITY TAIKU
+CT100/G/26152/25
+PROMPTING USER TO STORE DATA ELEMENTS
+*/
+#include <stdio.h>
+
+int main() {
+    // Declare and initialize the array
+    int scores[2][2] = {
+        {65, 92},
+        {84, 72}
+    };
+
+    // Print the array using nested loops
+    printf("The elements of the array are:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", scores[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
